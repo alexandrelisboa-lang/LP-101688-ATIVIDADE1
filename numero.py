6828f4804f14d1ad55bd19a711020a28de92c03b
alexandrelisboa-lang <alexandre.lisboa@ba.estudante.senai.br>
2026-03-04 15:31:37 -0300

    Add files via upload

---
+++ numero.py
@@ -0,0 +1,11 @@
+import os
+os.system("cls")
+
+#ENTRADA
+numero =int(input("digite o primeiro numero: "))
+
+if numero >=10 and numero <= 20:
+    print(" esta dento de intervalo entre 10 e 20")
+    
+else:
+    exit()
