5a697cb91b9de48d037505847b245aedb5742243
alexandrelisboa-lang <alexandre.lisboa@ba.estudante.senai.br>
2026-03-11 16:39:35 -0300

    Add files via upload

---
+++ dever.py
@@ -0,0 +1,47 @@
+import os
+os.system("cls")
+import time
+
+for i in range(1,20,2):
+  print(i)
+
+
+#for i in range(-1,-1,-1):
+  #  print(i)
+    #ESPERA DOIS SEGUNDO
+   # time.sleep(0.25)                           
+for i in range(100,-1,-1):
+    print(i)
+    #ESPERA DOIS SEGUNDO
+    time.sleep(0.5)                           
+                            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ numeros-pares.py
@@ -0,0 +1,6 @@
+import os
+os.system("cls")
+import time
+
+for i in range(1,20,2):
+  print(i)
